e741d8fffae9e45dd3ca3f34eb557847d04155e7
Benoit Jadinon <raz-l@raz-l.com>
2015-08-12 02:00:27 +0200

    ability to init from a custom Application, not only the first Activity

--- src/Acr.UserDialogs.Shared/UserDialogs.cs
+++ src/Acr.UserDialogs.Shared/UserDialogs.cs
@@ -1,39 +1,55 @@
 ﻿using System;
 
 
-namespace Acr.UserDialogs {
+namespace Acr.UserDialogs
+{
 
-    public static class UserDialogs {
+	public static class UserDialogs
+	{
 
-        static readonly Lazy<IUserDialogs> instance = new Lazy<IUserDialogs>(() => {
+		static readonly Lazy<IUserDialogs> instance = new Lazy<IUserDialogs> (() => {
 #if PCL
             throw new ArgumentException("This PCL library, not the platform library.  Did you include the nuget package in your project?");
 #elif __ANDROID__
-            throw new ArgumentException("In android, you must call UserDialogs.Init(Activity) from your first activity");
+			throw new ArgumentException ("In android, you must call UserDialogs.Init(Activity) from your first activity");
 #else
             return new UserDialogsImpl();
 #endif
-        });
+		});
 
-#if __ANDROID__
+		#if __ANDROID__
 
 
-        /// <summary>
-        /// Initialize android user dialogs
-        /// </summary>
-        public static void Init(Android.App.Activity activity, bool useAppCompat = false) {
-            ActivityLifecycleCallbacks.Register(activity);
-            if (useAppCompat)
-                Instance = new AppCompatUserDialogsImpl(null);
-            else
-                Instance = new UserDialogsImpl(null);
-        }
-#endif
+		/// <summary>
+		/// Initialize android user dialogs
+		/// </summary>
+		public static void Init (Android.App.Activity activity, bool useAppCompat = false)
+		{
+			ActivityLifecycleCallbacks.Register (activity);
+			if (useAppCompat)
+				Instance = new AppCompatUserDialogsImpl (null);
+			else
+				Instance = new UserDialogsImpl (null);
+		}
 
-        static IUserDialogs customInstance;
-        public static IUserDialogs Instance {
-            get { return customInstance ?? instance.Value; }
-            set { customInstance = value; }
-        }
-    }
+		/// <summary>
+		/// Initialize android user dialogs
+		/// </summary>
+		public static void Init (Android.App.Application application, bool useAppCompat = false)
+		{
+			ActivityLifecycleCallbacks.Register (application);
+			if (useAppCompat)
+				Instance = new AppCompatUserDialogsImpl (null);
+			else
+				Instance = new UserDialogsImpl (null);
+		}
+		#endif
+
+		static IUserDialogs customInstance;
+
+		public static IUserDialogs Instance {
+			get { return customInstance ?? instance.Value; }
+			set { customInstance = value; }
+		}
+	}
 }
