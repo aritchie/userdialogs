1c5b53a1814b423174fd8b3e754d18d6fddbe9f8
Allan Ritchie <allan.ritchie@gmail.com>
2015-08-14 22:40:07 -0400

    Update nuspec

--- nuspec/pack.cmd
+++ nuspec/pack.cmd
@@ -2,6 +2,6 @@
 del *.nupkg
 nuget pack Acr.UserDialogs.nuspec
 nuget pack Acr.UserDialogs.Android.AppCompat.nuspec
-rem nuget pack Acr.MvvmCross.Plugins.UserDialogs.nuspec
-rem nuget pack Acr.MvvmCross.Plugins.UserDialogs.AppCompat.nuspec
+nuget pack Acr.MvvmCross.Plugins.UserDialogs.nuspec
+nuget pack Acr.MvvmCross.Plugins.UserDialogs.AppCompat.nuspec
 pause
