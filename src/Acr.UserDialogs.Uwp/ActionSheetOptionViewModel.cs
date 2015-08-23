06eac7124c79d64938677670e9eb12d29c7891a7
Martijn van Dijk <mhvdijk@gmail.com>
2015-08-23 12:57:32 +0200

    Added UWP implementation

--- src/Acr.UserDialogs.Uwp/ActionSheetOptionViewModel.cs
+++ src/Acr.UserDialogs.Uwp/ActionSheetOptionViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Splat;
+using System;
 using System.Windows.Input;
 using Windows.UI.Xaml;
 
@@ -7,14 +8,16 @@ namespace Acr.UserDialogs {
 
     public class ActionSheetOptionViewModel {
 
-        public ActionSheetOptionViewModel(bool visible, string text, Action action) {
+        public ActionSheetOptionViewModel(bool visible, string text, Action action, IBitmap image = null) {
             this.Text = text;
             this.Action = new Command(action);
             this.Visible = visible ? Visibility.Visible : Visibility.Collapsed;
+            this.ItemIcon = image;
         }
 
         public Visibility Visible { get; }
         public string Text { get; }
         public ICommand Action { get; }
+        public IBitmap ItemIcon { get; }
     }
 }
