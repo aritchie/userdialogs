4d00a32e876b9e923985f74d03ee6d401d9f1bfb
Allan Ritchie <allan.ritchie@gmail.com>
2015-11-02 20:32:58 -0500

    update nuspec

--- nuspec/pack.cmd
+++ nuspec/pack.cmd
@@ -3,7 +3,7 @@ del *.nupkg
 rem stupid wp8 doesn't like 64bit and 32bit doesn't like c#6... my machine?
 rem %WinDir%\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe ../src/Acr.UserDialogs.sln /p:Platform="Any CPU" /property:Configuration=Release /t:Clean,Build
 nuget pack Acr.UserDialogs.nuspec
-rem nuget pack Acr.UserDialogs.Android.AppCompat.nuspec
+nuget pack Acr.UserDialogs.Android.AppCompat.nuspec
 rem nuget pack Acr.MvvmCross.Plugins.UserDialogs.nuspec
 rem nuget pack Acr.MvvmCross.Plugins.UserDialogs.AppCompat.nuspec
 rem nuget pack Acr.XamForms.UserDialogs.nuspec
