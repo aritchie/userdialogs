c14833428bebdbbc5c629bd16922b4a9e63a7e8e
Allan Ritchie <allan.ritchie@gmail.com>
2016-03-14 13:46:36 -0400

    Stub in testing library

---
+++ src/Acr.UserDialogs.Testing/UserDialogsImpl.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acr.UserDialogs
+{
+    public class UserDialogsImpl : AbstractUserDialogs
+    {
+        public override void Alert(AlertConfig config)
+        {
+        }
+
+
+        public override void ActionSheet(ActionSheetConfig config)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Confirm(ConfirmConfig config)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Prompt(PromptConfig config)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ShowError(string message, int timeoutMillis)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ShowSuccess(string message, int timeoutMillis)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Toast(ToastConfig config)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IProgressDialog CreateDialogInstance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ShowImage(IBitmap image, string message, int timeoutMillis)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Login(LoginConfig config)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
