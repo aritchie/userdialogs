1a4e93ef676fd6a50426a89b3a58705f40b07105
Allan Ritchie <allan.ritchie@gmail.com>
2016-04-15 15:41:11 -0400

    fix ios ipad actionsheet presentation

--- src/Acr.UserDialogs.iOS/UserDialogsImpl.cs
+++ src/Acr.UserDialogs.iOS/UserDialogsImpl.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Linq;
 using System.Timers;
+using Acr.Support.iOS;
 using UIKit;
 using BigTed;
+using CoreGraphics;
 using MessageBar;
 using Splat;
 
@@ -197,39 +199,26 @@ namespace Acr.UserDialogs
         }
 
 
-
-        protected virtual void Present(UIAlertView alert)
-        {
-            UIApplication.SharedApplication.InvokeOnMainThread(alert.Show);
-        }
-
-
         protected virtual void Present(UIAlertController alert)
         {
             var app = UIApplication.SharedApplication;
             app.InvokeOnMainThread(() =>
-                app.KeyWindow.RootViewController.PresentViewController(alert, true, null)
-            );
-            //    PresentInternal(app, controller, animated, action);
-            //else
-            //    app.InvokeOnMainThread(() => PresentInternal(app, controller, animated, action));
+            {
+                if (alert.PreferredStyle == UIAlertControllerStyle.ActionSheet && UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Pad)
+                {
+                    var top = app.GetTopViewController();
+
+                    var x = top.View.Bounds.Width / 2;
+                    var y = top.View.Bounds.Bottom;
+                    var rect = new CGRect(x, y, 0, 0);
+
+                    alert.PopoverPresentationController.SourceView = top.View;
+                    alert.PopoverPresentationController.SourceRect = rect;
+                    alert.PopoverPresentationController.PermittedArrowDirections = UIPopoverArrowDirection.Unknown;
+                }
+                app.KeyWindow.RootViewController.PresentViewController(alert, true, null);
+            });
         }
-
-
-
-        //static void PresentInternal(UIApplication app, UIViewController controller, bool animated, Action action) {
-        //    var top = app.GetTopViewController();
-        //    if (controller.PopoverPresentationController != null) {
-        //        var x = top.View.Bounds.Width / 2;
-        //        var y = top.View.Bounds.Bottom;
-        //        var rect = new CGRect(x, y, 0, 0);
-
-        //        controller.PopoverPresentationController.SourceView = top.View;
-        //        controller.PopoverPresentationController.SourceRect = rect;
-        //        controller.PopoverPresentationController.PermittedArrowDirections = UIPopoverArrowDirection.Unknown;
-        //    }
-        //    top.PresentViewController(controller, animated, action);
-        //}
 
 
         protected virtual void SetInputType(UITextField txt, InputType inputType)
