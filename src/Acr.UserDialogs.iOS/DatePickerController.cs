90ebad96cbf5fba86f068abfe0862349a110259d
Allan Ritchie <allan.ritchie@gmail.com>
2016-05-04 22:24:48 -0400

    add

---
+++ src/Acr.UserDialogs.iOS/DatePickerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UIKit;
+
+
+namespace Acr.UserDialogs
+{
+    public class DatePickerController : ModalDateTimePickerViewController
+    {
+        public DatePickerController(DatePromptConfig config, UIViewController parent) : base(config.Title, parent)
+        {
+            this.DatePicker = new UIDatePicker 
+            {
+                Mode = UIDatePickerMode.Date
+            };
+            this.Dismissed += (object sender, bool e) => 
+            {
+                
+//                config.OnResult?.Invoke(
+            };
+        }
+    }
+}
