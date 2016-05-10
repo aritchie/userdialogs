0f9473cdad8c4892e36cbe4662573d0a564dbcc0
Allan Ritchie <allan.ritchie@gmail.com>
2016-05-10 07:44:58 -0400

    looking into winform and wpf support

---
+++ src/Acr.UserDialogs.WindowsForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Acr.UserDialogs.WindowsForms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Acr.UserDialogs.Wpf/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Acr.UserDialogs.Wpf")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
