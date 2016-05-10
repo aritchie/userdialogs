023a626ef85115e22cdfb0604fc5b8e4223b0d41
Allan Ritchie <allan.ritchie@gmail.com>
2016-05-10 15:21:11 -0400

    fix a couple of compile f'ups from xam

--- src/Acr.UserDialogs.Android/Builders/DatePromptBuilder.cs
+++ src/Acr.UserDialogs.Android/Builders/DatePromptBuilder.cs
@@ -52,9 +52,11 @@ namespace Acr.UserDialogs.Builders
         }
 
 
+        static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
         public static long ToUnixTimestamp(this DateTime dateTime)
         {
-            return new DateTimeOffset(dateTime).ToUnixTimeMilliseconds();
+            var utc = TimeZoneInfo.ConvertTimeToUtc(dateTime);
+            return Convert.ToInt64((utc - Epoch).TotalSeconds);
         }
     }
 }
