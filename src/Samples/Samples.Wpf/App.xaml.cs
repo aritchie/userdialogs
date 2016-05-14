b98420a0bcfad4adbf6046cab648d8e1e8b10f66
Allan Ritchie <allan.ritchie@gmail.com>
2016-05-14 16:02:28 -0400

    WPF working

--- src/Samples/Samples.Wpf/App.xaml.cs
+++ src/Samples/Samples.Wpf/App.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+
 
 namespace Samples.Wpf
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
