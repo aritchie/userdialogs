fc8e36b208b62d181d65e790702c178fb00b319e
Allan Ritchie <allan.ritchie@gmail.com>
2016-05-28 08:08:00 -0400

    finally found original links - adding author credits

--- src/Acr.UserDialogs.iOS/ModalDateTimePickerViewController.cs
+++ src/Acr.UserDialogs.iOS/ModalDateTimePickerViewController.cs
@@ -1,4 +1,22 @@
-﻿// Slightly modified version of
+﻿// Slightly modified version of https://github.com/SharpMobileCode/ModalPickerViewController/blob/master/ModalPickerSample/ModalPickerSampleViewController.cs
+/*
+ * Copyright (C) 2014
+ * Author: Ruben Macias
+ * http://sharpmobilecode.com @SharpMobileCode
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 using System;
 using UIKit;
 using CoreGraphics;
