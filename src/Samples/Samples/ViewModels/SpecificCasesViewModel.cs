f8824754a65af179a76c32afad979b658c430a46
Allan Ritchie <allan.ritchie@gmail.com>
2016-07-05 22:26:55 -0400

    add specific use-cases page for user scenarios

---
+++ src/Samples/Samples/ViewModels/SpecificCasesViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Acr.UserDialogs;
+using Xamarin.Forms;
+
+
+namespace Samples.ViewModels
+{
+    public class SpecificCasesViewModel : AbstractViewModel
+    {
+        public SpecificCasesViewModel(IUserDialogs dialogs) : base(dialogs)
+        {
+            //this.LoadingTaskToAlert = new Command(async () =>
+            //{
+            //    using (this.Dialogs.Loading("Hold Up"))
+            //    {
+            //        await Task.Delay(TimeSpan.FromSeconds(2));
+            //    }
+            //    await this.Dialogs.AlertAsync("Do you see me?");
+            //});
+            this.LoadingTaskToAlert = new Command(() =>
+            {
+                this.Dialogs.ShowLoading("You really shouldn't use ShowLoading");
+                Task.Delay(TimeSpan.FromSeconds(2))
+                    .ContinueWith(x =>
+                    {
+                        this.Dialogs.HideLoading();
+                        this.Dialogs.Alert("Do you see me?");
+                    });
+            });
+        }
+
+
+        public ICommand LoadingTaskToAlert { get; }
+    }
+}
