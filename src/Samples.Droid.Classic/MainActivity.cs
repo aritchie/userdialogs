8228adc6adf07292cd3bffc7420d3dbceb09c3c6
Allan Ritchie <allan.ritchie@gmail.com>
2016-07-06 08:44:50 -0400

    Add droid classic (non appcompat) for testing

---
+++ src/Samples.Droid.Classic/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Acr.UserDialogs;
+using Android.App;
+using Android.OS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+namespace Samples.Droid.Classic
+{
+    [Activity(Label = "Samples.Droid.Classic", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            Forms.Init(this, bundle);
+
+            UserDialogs.Init(this);
+            this.LoadApplication(new App());
+        }
+    }
+}
+
