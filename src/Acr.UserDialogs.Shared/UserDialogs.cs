99db8f3c5b9404f9692bef3e6cfe834e98edc607
Allan Ritchie <allan.ritchie@gmail.com>
2016-07-12 09:05:27 -0400

    remove auto-init which can lead to downstream issues

--- src/Acr.UserDialogs.Shared/UserDialogs.cs
+++ src/Acr.UserDialogs.Shared/UserDialogs.cs
@@ -8,13 +8,12 @@ namespace Acr.UserDialogs {
 
     public static class UserDialogs {
 
-        static readonly Lazy<IUserDialogs> instance = new Lazy<IUserDialogs>(() => {
+        static readonly Lazy<IUserDialogs> instance = new Lazy<IUserDialogs>(() =>
+        {
 #if PCL
             throw new ArgumentException("This is the PCL library, not the platform library.  You must install the nuget package in your main executable/application project");
 #elif __ANDROID__
-            Init((Application)Application.Context.ApplicationContext);
-            return customInstance;
-            //throw new ArgumentException("In android, you must call UserDialogs.Init(Activity) from your first activity");
+            throw new ArgumentException("In android, you must call UserDialogs.Init(Activity) from your first activity OR UserDialogs.Init(App) from your custom application OR provide a factory function to get the current top activity via UserDialogs.Init(() => supply top activity)");
 #else
             return new UserDialogsImpl();
 #endif
@@ -25,7 +24,8 @@ namespace Acr.UserDialogs {
         /// <summary>
         /// Initialize android user dialogs
         /// </summary>
-        public static void Init(Func<Activity> topActivityFactory) {
+        public static void Init(Func<Activity> topActivityFactory)
+        {
             Instance = new UserDialogsImpl(topActivityFactory);
         }
 
@@ -33,7 +33,8 @@ namespace Acr.UserDialogs {
         /// <summary>
         /// Initialize android user dialogs
         /// </summary>
-        public static void Init(Application app) {
+        public static void Init(Application app)
+        {
             ActivityLifecycleCallbacks.Register(app);
             Init(() => ActivityLifecycleCallbacks.CurrentTopActivity);
         }
@@ -42,14 +43,16 @@ namespace Acr.UserDialogs {
         /// <summary>
         /// Initialize android user dialogs
         /// </summary>
-        public static void Init(Activity activity) {
+        public static void Init(Activity activity)
+        {
             ActivityLifecycleCallbacks.Register(activity);
             Init(() => ActivityLifecycleCallbacks.CurrentTopActivity);
         }
 #endif
 
         static IUserDialogs customInstance;
-        public static IUserDialogs Instance {
+        public static IUserDialogs Instance
+        {
             get { return customInstance ?? instance.Value; }
             set { customInstance = value; }
         }
