1a51f4e341ed07b0833d9dc4f1c37daab34c90da
Allan Ritchie <allan.ritchie@gmail.com>
2016-08-02 16:48:17 -0400

    Add test case for two selected dates

--- src/Samples/Samples/ViewModels/SpecificCasesViewModel.cs
+++ src/Samples/Samples/ViewModels/SpecificCasesViewModel.cs
@@ -11,23 +11,29 @@ namespace Samples.ViewModels
     {
         public SpecificCasesViewModel(IUserDialogs dialogs) : base(dialogs)
         {
-            //this.LoadingTaskToAlert = new Command(async () =>
-            //{
-            //    using (this.Dialogs.Loading("Hold Up"))
-            //    {
-            //        await Task.Delay(TimeSpan.FromSeconds(2));
-            //    }
-            //    await this.Dialogs.AlertAsync("Do you see me?");
-            //});
             this.LoadingTaskToAlert = new Command(() =>
             {
                 this.Dialogs.ShowLoading("You really shouldn't use ShowLoading");
                 Task.Delay(TimeSpan.FromSeconds(2))
                     .ContinueWith(x => this.Dialogs.Alert("Do you see me?"));
             });
+
+            this.TwoDatePickers = new Command(async () =>
+            {
+                var v1 = await this.Dialogs.DatePromptAsync("Date 1 (Past -1 Day)", DateTime.Now.AddDays(-1));
+                if (!v1.Ok)
+                    return;
+
+                var v2 = await this.Dialogs.DatePromptAsync("Date 2 (Future +1 Day)", DateTime.Now.AddDays(1));
+                if (!v2.Ok)
+                    return;
+
+                this.Dialogs.Alert($"Date 1: {v1.SelectedDate} - Date 2: {v2.SelectedDate}");
+            });
         }
 
 
         public ICommand LoadingTaskToAlert { get; }
+        public ICommand TwoDatePickers { get; }
     }
 }
