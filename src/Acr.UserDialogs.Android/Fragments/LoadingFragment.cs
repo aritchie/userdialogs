2c68ba879ac9c2dab343b160c814861274c1c180
Allan Ritchie <allan.ritchie@gmail.com>
2016-08-05 14:46:01 -0400

    comment out for now

--- src/Acr.UserDialogs.Android/Fragments/LoadingFragment.cs
+++ src/Acr.UserDialogs.Android/Fragments/LoadingFragment.cs
@@ -1,49 +1,48 @@
-using System;
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Support.V7.App;
-using AndroidHUD;
+//using System;
+//using Android.Content;
+//using Android.OS;
+//using Android.Support.V7.App;
+//using AndroidHUD;
 
 
-namespace Acr.UserDialogs.Fragments
-{
-    public class LoadingFragment : AppCompatDialogFragment
-    {
-        public ProgressDialog Config { get; set; }
+//namespace Acr.UserDialogs.Fragments
+//{
+//    public class LoadingFragment : AppCompatDialogFragment
+//    {
+//        public ProgressDialog Config { get; set; }
 
 
-        public override void OnSaveInstanceState(Bundle bundle)
-        {
-            base.OnSaveInstanceState(bundle);
-            ConfigStore.Instance.Store(bundle, this.Config);
-        }
+//        public override void OnSaveInstanceState(Bundle bundle)
+//        {
+//            base.OnSaveInstanceState(bundle);
+//            ConfigStore.Instance.Store(bundle, this.Config);
+//        }
 
 
-        public override void OnViewStateRestored(Bundle bundle)
-        {
-            base.OnViewStateRestored(bundle);
-            if (this.Config == null)
-                this.Config = ConfigStore.Instance.Pop<ProgressDialog>(bundle);
-        }
+//        public override void OnViewStateRestored(Bundle bundle)
+//        {
+//            base.OnViewStateRestored(bundle);
+//            if (this.Config == null)
+//                this.Config = ConfigStore.Instance.Pop<ProgressDialog>(bundle);
+//        }
 
 
-        public override void OnAttach(Context context)
-        {
-            base.OnAttach(context);
-            if (this.Config.IsShowing)
-                this.Config.Show();
-        }
+//        public override void OnAttach(Context context)
+//        {
+//            base.OnAttach(context);
+//            if (this.Config.IsShowing)
+//                this.Config.Show();
+//        }
 
 
-        public override void OnDetach()
-        {
-            base.OnDetach();
-            try
-            {
-                AndHUD.Shared.Dismiss(this.Activity);
-            }
-            catch { }
-        }
-    }
-}
+//        public override void OnDetach()
+//        {
+//            base.OnDetach();
+//            try
+//            {
+//                AndHUD.Shared.Dismiss(this.Activity);
+//            }
+//            catch { }
+//        }
+//    }
+//}
