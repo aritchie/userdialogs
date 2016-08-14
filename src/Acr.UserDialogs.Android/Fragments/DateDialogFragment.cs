618140dbd79c846afa7a24aaecee7fac5f6a751e
Allan Ritchie <allan.ritchie@gmail.com>
2016-08-13 22:18:05 -0400

    fix #232 - keyboard becomes visble for date/time dialogs

--- src/Acr.UserDialogs.Android/Fragments/DateDialogFragment.cs
+++ src/Acr.UserDialogs.Android/Fragments/DateDialogFragment.cs
@@ -24,6 +24,13 @@ namespace Acr.UserDialogs.Fragments
         }
 
 
+        protected override void SetDialogDefaults(Dialog dialog)
+        {
+            base.SetDialogDefaults(dialog);
+            dialog.Window.SetSoftInputMode(SoftInput.StateHidden);
+        }
+
+
         protected override Dialog CreateDialog(DatePromptConfig config)
         {
             return DatePromptBuilder.Build(this.Activity, config);
@@ -45,6 +52,14 @@ namespace Acr.UserDialogs.Fragments
                 this.Config?.OnAction?.Invoke(new DatePromptResult(false, DateTime.MinValue));
                 this.Dismiss();
             }
+        }
+
+
+        protected override void SetDialogDefaults(Dialog dialog)
+        {
+            dialog.SetCancelable(false);
+            dialog.SetCanceledOnTouchOutside(false);
+            dialog.KeyPress += this.OnKeyPress;
         }
 
 
