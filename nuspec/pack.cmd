c2ed86af467a5c94ebcb1cdf6ec94f62f1f0a139
Allan Ritchie <allan.ritchie@gmail.com>
2016-10-15 19:52:13 -0400

    push

--- nuspec/pack.cmd
+++ nuspec/pack.cmd
@@ -1,6 +1,6 @@
 @echo off
 del *.nupkg
 nuget pack Acr.UserDialogs.nuspec
-nuget pack Plugin.UserDialogs.nuspec
+rem nuget pack Plugin.UserDialogs.nuspec
 rem nuget pack Acr.UserDialogs.WindowsForms.nuspec
 pause
