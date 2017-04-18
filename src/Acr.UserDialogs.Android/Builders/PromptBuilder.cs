bfd5211953b8ea5b12bc70ddbec2c62ad2298978
Allan Ritchie <allan.ritchie@gmail.com>
2017-04-18 11:50:11 -0400

    fix decimal parsing for other locales

--- src/Acr.UserDialogs.Android/Builders/PromptBuilder.cs
+++ src/Acr.UserDialogs.Android/Builders/PromptBuilder.cs
@@ -162,7 +162,7 @@ namespace Acr.UserDialogs.Builders
                 case InputType.DecimalNumber:
                     txt.InputType = InputTypes.ClassNumber | InputTypes.NumberFlagDecimal | InputTypes.NumberFlagSigned;
                     txt.SetSingleLine(true);
-                    txt.KeyListener = DigitsKeyListener.GetInstance(String.Format("1234567890{0}", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator));
+                    txt.KeyListener = DigitsKeyListener.GetInstance("1234567890" + CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
                     break;
 
                 case InputType.Email:
@@ -181,8 +181,8 @@ namespace Acr.UserDialogs.Builders
                     break;
 
                 case InputType.NumericPassword:
-                    txt.InputType = InputTypes.ClassNumber;
                     txt.TransformationMethod = PasswordTransformationMethod.Instance;
+                    txt.InputType = InputTypes.ClassNumber;
                     break;
 
                 case InputType.Password:
