0263711d46644edf7c6e26e717c695319851c427
Allan Ritchie <allan.ritchie@gmail.com>
2017-05-31 21:54:21 -0400

    macOS framed

--- nuspec/pack.cmd
+++ nuspec/pack.cmd
@@ -1,6 +1,4 @@
 @echo off
 del *.nupkg
 nuget pack Acr.UserDialogs.nuspec
-rem nuget pack Plugin.UserDialogs.nuspec
-rem nuget pack Acr.UserDialogs.WindowsForms.nuspec
 pause
---
+++ src/Acr.UserDialogs.macOS/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle ("Acr.UserDialogs.macOS")]
+[assembly: AssemblyDescription ("")]
