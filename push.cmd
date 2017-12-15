59efe6bce35d4aabfd8e89057d9ae1eae3811900
Allan Ritchie <allan.ritchie@gmail.com>
2017-12-15 16:25:57 -0500

    SDK style project!!

--- push.cmd
+++ push.cmd
@@ -1,4 +1,5 @@
 @echo off
 copy *.nupkg %HOMEPATH%\dropbox\nuget\ /y
 nuget push *.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push .\src\Acr.UserDialogs\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
 pause
--- src/Acr.UserDialogs.Android/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Acr.UserDialogs.Android")]
-[assembly: AssemblyDescription("")]
