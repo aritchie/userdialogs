867403f7bca66f015cb5a967c5e94d39c7475aca
Allan Ritchie <allan.ritchie@gmail.com>
2018-03-18 17:29:49 -0400

    clean up static factory

--- src/Acr.UserDialogs/Platforms/Shared/UserDialogs.cs
+++ src/Acr.UserDialogs/Platforms/Shared/UserDialogs.cs
@@ -1,117 +1,23 @@
 ﻿using System;
-using Acr.UserDialogs.Infrastructure;
-#if __IOS__
-using UIKit;
-#endif
-#if __ANDROID__
-using Android.App;
-#endif
-#if __TIZEN__
-using ElmSharp;
-#endif
+
 
 namespace Acr.UserDialogs
 {
-
-    public static class UserDialogs
+    public static partial class UserDialogs
     {
-
-#if __ANDROID__
-
-        /// <summary>
-        /// Initialize android user dialogs
-        /// </summary>
-        public static void Init(Func<Activity> topActivityFactory)
-        {
-            Instance = new UserDialogsImpl(topActivityFactory);
-        }
-
-
-        /// <summary>
-        /// Initialize android user dialogs
-        /// </summary>
-        public static void Init(Application app)
-        {
-            ActivityLifecycleCallbacks.Register(app);
-            Init(() => ActivityLifecycleCallbacks.CurrentTopActivity);
-        }
-
-
-        /// <summary>
-        /// Initialize android user dialogs
-        /// </summary>
-        public static void Init(Activity activity)
-        {
-            ActivityLifecycleCallbacks.Register(activity);
-            Init(() => ActivityLifecycleCallbacks.CurrentTopActivity);
-        }
-#elif __IOS__
-
-        /// <summary>
-        /// OPTIONAL: Initialize iOS user dialogs with your top viewcontroll function
-        /// </summary>
-        public static void Init(Func<UIViewController> viewControllerFunc)
-        {
-            Instance = new UserDialogsImpl(viewControllerFunc);
-        }
-#elif WINDOWS_UWP
-
-        public static Action<Action> Dispatch { get; set; } => new Action(() => {
-})
-        /// <summary>
-        /// Initialize UWP user dialogs
-        /// </summary>
-        public static void Init(Action customDispatcher = null)
-        {
-    /*
-            try
-            {
-                if (LockApplicationHost.GetForCurrentView() != null)
-                {
-                    CoreApplication.GetCurrentView().Dispatcher;
-                }
-            }
-            catch
-            {
-                CoreApplication.MainView.CoreWindow.Dispatcher;
-            }
-     */
-            Instance = new UserDialogsImpl();
-        }
-
-#elif __TIZEN__
-        /// <summary>
-        /// Initialize Tizen user dialogs
-        /// </summary>
-        /// <param name="window"></param>
-        public static void Init(Window window)
-        {
-            Instance = new UserDialogsImpl(window);
-        }
-#endif
-
+        #if NETSTANDARD
         static IUserDialogs currentInstance;
         public static IUserDialogs Instance
         {
             get
             {
-#if BAIT
                 if (currentInstance == null)
                     throw new ArgumentException("[Acr.UserDialogs] This is the bait library, not the platform library.  You must install the nuget package in your main executable/application project");
-#elif __ANDROID__
-                if (currentInstance == null)
-                    throw new ArgumentException("[Acr.UserDialogs] In android, you must call UserDialogs.Init(Activity) from your first activity OR UserDialogs.Init(App) from your custom application OR provide a factory function to get the current top activity via UserDialogs.Init(() => supply top activity)");
-#elif __TIZEN__
-                if (currentInstance == null)
-                {
-                    throw new ArgumentException("[Acr.UserDialogs] In Tizen, the window instance of your custom application must be passed by using UserDialogs.Init(Window).");
-                }
-#else
-                currentInstance = currentInstance ?? new UserDialogsImpl();
-#endif
+
                 return currentInstance;
             }
             set => currentInstance = value;
         }
+        #endif
     }
 }
