3bb19a5459f87d9f06e036f0bd21d780f224115c
Dimitri Dokadze <dimitri@vehicletracking.com>
2018-05-09 09:22:33 -0400

    Rewrote TimePicker using TimePickerDialog for better styling support

--- src/Acr.UserDialogs/Platforms/Android/Builders/TimePromptBuilder.cs
+++ src/Acr.UserDialogs/Platforms/Android/Builders/TimePromptBuilder.cs
@@ -11,42 +11,16 @@ namespace Acr.UserDialogs.Builders
     {
         public static Dialog Build(Activity activity, TimePromptConfig config)
         {
-            var picker = new TimePicker(activity);
-            var builder = new AlertDialog.Builder(activity, config.AndroidStyleId ?? 0)
-                .SetCancelable(false)
-                .SetTitle(config.Title)
-                .SetView(picker);
+            var themeId = config.AndroidStyleId ?? 0;
+            var is24Hour = config.Use24HourClock ?? DateFormat.Is24HourFormat(activity);
 
-            if (config.SelectedTime != null)
-            {
-                picker.CurrentHour = new Integer(config.SelectedTime.Value.Hours);
-                picker.CurrentMinute = new Integer(config.SelectedTime.Value.Minutes);
-            }
+            var timePickerDialog = new TimePickerDialog(activity, themeId, (sender, args) =>
+              {
+                  var ts = new TimeSpan(0, args.HourOfDay, args.Minute, 0);
+                  config.OnAction?.Invoke(new TimePromptResult(true, ts));
+              }, config.SelectedTime.Value.Hours, config.SelectedTime.Value.Minutes, is24Hour);
 
-            var is24Hour = config.Use24HourClock ?? DateFormat.Is24HourFormat (activity);
-            picker.SetIs24HourView(new Java.Lang.Boolean(is24Hour));
-
-            if (config.IsCancellable)
-            {
-                builder.SetNegativeButton(
-                    config.CancelText,
-                    (sender, args) =>
-                    {
-                        var ts = new TimeSpan(0, picker.CurrentHour.IntValue(), picker.CurrentMinute.IntValue(), 0);
-                        config.OnAction?.Invoke(new TimePromptResult(false, ts));
-                    }
-                );
-            }
-            builder.SetPositiveButton(
-                config.OkText,
-                (sender, args) =>
-                {
-                    var ts = new TimeSpan(0, picker.CurrentHour.IntValue(), picker.CurrentMinute.IntValue(), 0);
-                    config.OnAction?.Invoke(new TimePromptResult(true, ts));
-                }
-            );
-
-            return builder.Show();
+            return timePickerDialog;
         }
     }
 }
