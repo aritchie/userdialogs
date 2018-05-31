7536377391ba8e616ad0d493aee604a1720daa8c
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2018-06-01 00:43:23 +0300

    base implementation

---
+++ src/Acr.UserDialogs/Platforms/WPF/UserDialogs.cs
@@ -0,0 +1,17 @@
+﻿namespace Acr.UserDialogs
+{
+    public static partial class UserDialogs
+    {
+        private static IUserDialogs currentInstance;
+
+        public static IUserDialogs Instance
+        {
+            get
+            {
+                currentInstance = currentInstance ?? new UserDialogsImpl();
+                return currentInstance;
+            }
+            set => currentInstance = value;
+        }
+    }
+}
---
+++ src/Acr.UserDialogs/Platforms/WPF/UserDialogsImpl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Acr.UserDialogs
+{
+    public class UserDialogsImpl : AbstractUserDialogs
+    {
+        public override Task AlertAsync(string message, string title = null, string okText = null,
+            CancellationToken? cancelToken = null)
+        {
+            MessageBox.Show(message, title);
+            return Task.FromResult(true);
+        }
+
+        public override IDisposable Toast(string title, TimeSpan? dismissTimer = null)
+        {
+            MessageBox.Show(title);
+            return null;
+        }
+    }
+}
