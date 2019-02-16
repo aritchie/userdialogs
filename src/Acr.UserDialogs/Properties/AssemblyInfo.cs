69e7cdf9eaab2043ba0efb7ab245fdb75548116a
Allan Ritchie <allan.ritchie@gmail.com>
2019-02-16 09:46:15 -0500

    more fixes to project

--- src/Acr.UserDialogs/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Acr.UserDialogs")]
-[assembly: AssemblyDescription("Allows for messagebox style dialogs to be called from your shared/PCL/MVVM code\r\n- Action Sheet (multiple choice menu)\r\n- Alert\r\n- Confirm\r\n- Date\r\n- Loading\r\n- Login\r\n- Progress\r\n- Prompt\r\n- Toast (Snackbars)\r\n- Time\r\n\r\nOn android, be sure to call UserDialogs.Init() before using!\r\n\r\nSupported Platforms\r\n- iOS 8+\r\n- Android\r\n- Universal Windows Platform (Win10/UWP)\r\n- NET Standard 2\r\n- PCL 259")]
