471c16e1c7866beab5642842123719b0150b4932
Patrick Decoster <p.decoster@vogels.com>
2021-01-08 13:01:10 +0100

    Implemented AutoCorrectionConfig on Android Prompt

--- src/Samples/Samples/ViewModels/StandardViewModel.cs
+++ src/Samples/Samples/ViewModels/StandardViewModel.cs
@@ -174,6 +174,19 @@ namespace Samples.ViewModels
                 },
                 new CommandViewModel
                 {
+                    Text = "Prompt NoAutoCorrect",
+                    Command = new Command(async () =>
+                    {
+                        var result = await this.Dialogs.PromptAsync(new PromptConfig
+                        {
+                            Title = "Prompt NoAutoCorrect",
+                            Message = "When entering text, the keyboard should not provide autocorrect options",
+                            AutoCorrectionConfig = AutoCorrectionConfig.No
+                        });
+                    })
+                },
+                new CommandViewModel
+                {
                     Text = "Prompt AutoCorrect",
                     Command = new Command(async () =>
                     {
