82cb0f099afa3b6f759bb0226992050f0b90f8f1
Allan Ritchie <allan.ritchie@gmail.com>
2021-11-18 23:02:12 -0500

    up the uap requirement

--- sample/Samples.iOS/Main.cs
+++ sample/Samples.iOS/Main.cs
@@ -10,7 +10,7 @@ namespace Samples.iOS
 
         static void Main(string[] args)
         {
-            UIApplication.Main(args, null, "AppDelegate");
+            UIApplication.Main(args, null, typeof(AppDelegate));
         }
     }
 }
