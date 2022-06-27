fa1b3708a8618012786ec2591cfd6f188f81266c
Allan Ritchie <allan.ritchie@gmail.com>
2022-06-26 22:04:26 -0400

    Fixes & add maccatalyst

--- sample/Sample/MauiProgram.cs
+++ sample/Sample/MauiProgram.cs
@@ -1,29 +1,28 @@
 ﻿using Acr.UserDialogs;
 using Microsoft.Maui.LifecycleEvents;
 
-namespace Sample
-{
-    public static class MauiProgram
-    {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                })
-                .ConfigureLifecycleEvents(events =>
-                {
-#if ANDROID
-                    events.AddAndroid(android => android.OnApplicationCreate(app => UserDialogs.Init(app)));
-#endif
-                })
-                ;
+namespace Sample;
 
-            return builder.Build();
-        }
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+            .ConfigureLifecycleEvents(events =>
+            {
+#if ANDROID
+                events.AddAndroid(android => android.OnApplicationCreate(app => UserDialogs.Init(app)));
+#endif
+            });
+
+        return builder.Build();
     }
 }
