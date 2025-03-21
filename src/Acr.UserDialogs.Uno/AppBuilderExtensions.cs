f63f2b6cf815d86285786c312a172466919f7a0f
Allan Ritchie <allan.ritchie@gmail.com>
2025-03-21 12:52:48 -0400

    add uno user dialogs

---
+++ src/Acr.UserDialogs.Uno/AppBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Acr.UserDialogs;
+
+
+public static class AppBuilderExtensions
+{
+    public static IApplicationBuilder AddUserDialogs(this IApplicationBuilder builder)
+        => builder.Configure((host, _) => host.AddUserDialogs());
+
+    public static IHostBuilder AddUserDialogs(this IHostBuilder builder)
+        => builder.ConfigureServices(services =>
+        {
+            services.AddSingleton(UserDialogs.Instance);
+            #if __ANDROID__
+            // hope for the best casting for now
+            UserDialogs.Init(() => (Android.App.Activity)ContextHelper.Current);
+            #endif
+        });
+}
+
